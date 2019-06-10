8bb97f724552b6befc23d942e07a44701aba204a
Anthony Lockett <alockettjr@me.com>
2019-06-10 18:36:27 -0400

    Initial Commit

---
+++ Video Poker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video Poker
+//
+//  Created by Anthony Lockett on 6/10/19.
+//  Copyright © 2019 Anthony Lockett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
