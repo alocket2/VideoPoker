90c4fc3b9d85b9a16def780e57c8883056ff6cba
Anthony Lockett <alockettjr@me.com>
2019-06-10 20:26:22 -0400

    Added extension to Array

---
+++ Video Poker/Extensions/Array.swift
@@ -0,0 +1,23 @@
+//
+//  Array.swift
+//  Video Poker
+//
+//  Created by Anthony Lockett on 6/10/19.
+//  Copyright © 2019 Anthony Lockett. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element == Card {
+
+    mutating func shuffle() -> Deck {
+        var shuffledDeck: Deck = []
+        for _ in self {
+            let random = Int(arc4random_uniform(UInt32(self.count - 1)))
+            shuffledDeck.append(self[random])
+            self.remove(at: random)
+        }
+        
+        return shuffledDeck
+    }
+}
